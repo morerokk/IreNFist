289f5f46b0cc1a009bbf576a3ba0442074dd1bc0
Hugo Zink <hugo.zink@hotmail.com>
2020-07-25 16:41:20 +0200

    Tweaked copbrain prehook to also notify copmovement

--- IRE AND FIST REBORN/lua/copbrain.lua
+++ IRE AND FIST REBORN/lua/copbrain.lua
@@ -2,6 +2,7 @@
 -- This function is called to sync the units to late joiners. To prevent possible crashes, substitute a default team if none is set.
 Hooks:PreHook(CopBrain, "save", "inf_setbraincopteam", function(self, save_data)
 	if not self._logic_data.team then
-		self:on_team_set(managers.groupai:state()._teams[tweak_data.levels:get_default_team_ID(self._unit:base():char_tweak().access == "gangster" and "gangster" or "combatant")])
+		-- This function on CopMovement will also apply the changes to CopBrain
+		self:movement():set_team(managers.groupai:state()._teams[tweak_data.levels:get_default_team_ID(self._unit:base():char_tweak().access == "gangster" and "gangster" or "combatant")])
 	end
 end)
--- IRE AND FIST REBORN/lua/copmovement.lua
+++ IRE AND FIST REBORN/lua/copmovement.lua
@@ -1,9 +1,7 @@
 -- Sometimes HRT's spawn without a team set which crashes the game
 -- Set a default team for cop units if they dont have a team
 Hooks:PreHook(CopMovement, "team", "inf_setcopteamifnoteam", function(self)
-
     if not self._team then
         self:set_team(managers.groupai:state()._teams[tweak_data.levels:get_default_team_ID(self._unit:base():char_tweak().access == "gangster" and "gangster" or "combatant")])
     end
-
 end)

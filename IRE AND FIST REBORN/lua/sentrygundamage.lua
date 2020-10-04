b832012780fa64a05e9edd28b8b9736216b8900d
Hugo Zink <hugo.zink@hotmail.com>
2020-10-04 19:00:18 +0200

    Turrets dying is now always properly synced
    If a turret dies, the game will now forcibly send a network message through BLT networking. Other peers with InF installed will see this message and will apply the death locally.

---
+++ IRE AND FIST REBORN/lua/sentrygundamage.lua
@@ -0,0 +1,46 @@
+-- Force the turret to die on the clients' sides when it is killed
+Hooks:PostHook(SentryGunDamage, "die", "inf_swatturret_dead_sync_death_manually", function(self)
+
+    -- Don't do anything to regular sentries
+    local turret_units = managers.groupai:state():turrets()
+    if not turret_units or not table.contains(turret_units, self._unit) then
+        return
+    end
+
+    if not self._unit or not self._unit:id() then
+        return
+    end
+
+    -- Notify clients that this turret is dead
+    LuaNetworking:SendToPeers("inf_turretdead", tostring(self._unit:id()))
+end)
+
+-- Listen for "turret died" notifications and apply the death locally
+Hooks:Add("NetworkReceivedData", "NetworkReceivedData_InF_SwatTurretDied", function(sender, messageType, data)
+    if messageType ~= "inf_turretdead" or not data then
+        return
+    end
+
+    local turret_unit_id = tonumber(data)
+    if not turret_unit_id then
+        return
+    end
+
+    local turret_units = managers.groupai:state():turrets()
+    if not turret_units then
+        return
+    end
+
+    for i, unit in pairs(turret_units) do
+        if unit:id() == turret_unit_id then
+            -- If the turret already died, don't worry about it
+            if not alive(unit) then
+                return
+            end
+
+            -- Probably safe to call without parameters, the game does it on load() too
+            turret_unit:character_damage():die()
+            break
+        end
+    end
+end)

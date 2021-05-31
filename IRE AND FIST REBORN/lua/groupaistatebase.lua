71964f406a0e4371d02152c83e451fbe4ad34a25
Hugo Zink <hugo.zink@hotmail.com>
2021-05-31 19:14:52 +0200

    Fixed attention objects being unnecessarily registered

--- IRE AND FIST REBORN/lua/groupaistatebase.lua
+++ IRE AND FIST REBORN/lua/groupaistatebase.lua
@@ -148,14 +148,19 @@ end
 -- For compatibility reasons, this feature is also disabled if "Think Faster" is disabled in InF's options.
 -- There is usually no need to *not* play with this fix, so that is not recommended unless you have another mod that fixes this already.
 if not IreNFist.mod_compatibility.think_faster and InFmenu.settings.thinkfaster then
+
 	-- Holds removed attention objects. This is necessary for maps that might switch from loud back to stealth.
 	local removed_attention_objects = {}
+
+	local function is_attention_obj_unnecessary_for_loud(attention_object)
+		return not attention_object.nav_tracker and not attention_object.unit:vehicle_driving() or attention_object.unit:in_slot(1) or attention_object.unit:in_slot(17) and attention_object.unit:character_damage()
+	end
 
 	local function unregister_attention_objects(self)
 		local ai_attention_objects = self:get_all_AI_attention_objects()
 
 		for u_key, attention_object in pairs(ai_attention_objects) do
-			if not attention_object.nav_tracker and not attention_object.unit:vehicle_driving() or attention_object.unit:in_slot(1) or attention_object.unit:in_slot(17) and attention_object.unit:character_damage() then
+			if is_attention_obj_unnecessary_for_loud(attention_object) then
 				removed_attention_objects[u_key] = attention_object
 				attention_object.handler:set_attention(nil)
 			end
@@ -193,4 +198,30 @@ if not IreNFist.mod_compatibility.think_faster and InFmenu.settings.thinkfaster 
 			unregister_attention_objects(self)
 		end
 	end)
+
+	-- Additionally, when a new object is registered, check if it should actually be registered or not.
+	local register_attention_obj_orig = GroupAIStateBase.register_AI_attention_object
+	function GroupAIStateBase:register_AI_attention_object(unit, handler, nav_tracker, team, SO_access)
+		-- In stealth, always register the object normally
+		if managers.groupai:state():whisper_mode() then
+			return register_attention_obj_orig(self, unit, handler, nav_tracker, team, SO_access)
+		end
+
+		local attention_obj = {
+			unit = unit,
+			handler = handler,
+			nav_tracker = nav_tracker,
+			team = team,
+			SO_access = SO_access
+		}
+
+		if is_attention_obj_unnecessary_for_loud(attention_obj) then
+			-- Object is unnecessary, add it to the local table of removed objects so that it could still be re-added later
+			removed_attention_objects[unit:key()] = attention_obj
+		else
+			-- Object is necessary for loud, register it normally
+			return register_attention_obj_orig(self, unit, handler, nav_tracker, team, SO_access)
+		end    
+	end
+
 end
